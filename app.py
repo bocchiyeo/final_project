6d72f86481217304b3c5f3e2c8ff9a5315a287a1
bocchiyeo <hweehngyeo@yahoo.com.sg>
2025-03-09 19:45:39 +0800

    WIP

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, request, render_template, redirect
+from quart import Quart, request, render_template
+from mercapi import Mercapi
 
-app = Flask(__name__)
+m = Mercapi()
+
+app = Quart(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+async def index():
+    s = request.args.get("q")
+    if s:
+        result = await m.search(s)
+        if result.items:
+            first_item = result.items[0]
+            print(f"Type of first item: {type(first_item)}")
+            print(f"Attributes and methods of first item: {dir(first_item)}")
+            print(f"First item data: {first_item.__dict__}")
+        return await render_template("search.html", result=result)
+    return await render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
